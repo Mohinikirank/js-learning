cb33b2381842a09b0df88c48622e1229e4b7b458
Mohinikirank <mohinikirank22@gmail.com>
2024-03-08 15:36:28 +0530

    Variables and datatypes
    Variables and datatypes

---
+++ 01_variableAssign.js
@@ -0,0 +1,36 @@
+console.log("======step 1======");
+
+var myFullName;
+myFullName="Mohini Kadam";
+console.log("My Full Name before initialization:",myFullName);
+
+myFullName="Mohini Kiran Kadam";
+console.log("My Full Name After update:",myFullName);
+
+console.log("======step 2======");
+
+var myLove=" I Love Only JS";
+console.log("MY Love:",myLove);
+
+console.log("======step 3======");
+
+var firstName="Mohini";
+console.log("First Name:",firstName);
+
+var lastName="Kadam";
+console.log("Last Name:",lastName);
+
+var age="26";
+console.log("Age:",age);
+
+var address="Iyyapanthangal,Chennai";
+console.log("Address:",address);
+
+var pinCode="600056";
+console.log("Pincode:",pinCode);
+
+var gender="Female";
+console.log("Gender:",gender);
+
+var maritalStatus="Married";
+console.log("Marital Status:",maritalStatus);
